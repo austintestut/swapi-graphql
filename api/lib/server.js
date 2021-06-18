7db41540479a4dbb78ef1a02c5b668654f0433fb
Austin Testut <austintestut@gmail.com>
2021-06-18 16:21:00 -0500

    fixed for server port issue

---
+++ api/lib/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const app = require('./service');
+
+// Listen for incoming HTTP requests
+const listener = app.listen(8080 || undefined, () => {
+  let host = listener.address().address;
+  if (host === '::') {
+    host = 'localhost';
+  }
+  const port = listener.address().port;
+  // eslint-disable-next-line no-console
+  console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
+});
